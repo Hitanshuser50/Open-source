e497e554bdf6df9c069350bf65832271a133d601
Hitanshuser50 <122915291+Hitanshuser50@users.noreply.github.com>
2023-08-03 19:54:42 +0530

    Time: 1 ms (78.73%), Space: 41.2 MB (64.44%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        if (digits.isEmpty()) return result;
+        
+        Map<Character, String> mapping = new HashMap<>();
+        mapping.put('2', "abc");
+        mapping.put('3', "def");
+        mapping.put('4', "ghi");
+        mapping.put('5', "jkl");
+        mapping.put('6', "mno");
+        mapping.put('7', "pqrs");
+        mapping.put('8', "tuv");
+        mapping.put('9', "wxyz");
+        
+        StringBuilder currentCombination = new StringBuilder();
+        backtrack(digits, 0, mapping, currentCombination, result);
+        
+        return result;
+    }
+    
+    private void backtrack(String digits, int index, Map<Character, String> mapping, StringBuilder currentCombination, List<String> result) {
+        if (index == digits.length()) {
+            result.add(currentCombination.toString());
+            return;
+        }
+        
+        char digit = digits.charAt(index);
+        String letters = mapping.get(digit);
+        for (char letter : letters.toCharArray()) {
+            currentCombination.append(letter);
+            backtrack(digits, index + 1, mapping, currentCombination, result);
+            currentCombination.deleteCharAt(currentCombination.length() - 1); // Backtrack by removing the last letter added
+        }
+    }
+}
